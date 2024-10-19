6649d14b35cee918d38d9bf9e4bdec7e018d5989
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-10-19 16:09:24 +0000

    added vars stddb

--- stddb.py
+++ stddb.py
@@ -2,13 +2,14 @@ import pymysql
 import os
 
 timeout = 10
+my_secret = os.environ['password']
 connection = pymysql.connect(
   charset="utf8mb4",
   connect_timeout=timeout,
   cursorclass=pymysql.cursors.DictCursor,
   db="defaultdb",
   host="mysql-smartsys-smartsys-db.k.aivencloud.com",
-  password=os.environ['password'],
+  password=my_secret,
   read_timeout=timeout,
   port=24350,
   user="avnadmin",
