4d7bae56cb3d831a0c0967809c9ae51031c67eb1
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-10-19 16:38:40 +0000

    pass

--- stddb.py
+++ stddb.py
@@ -1,15 +1,13 @@
 import pymysql
-import os
 
 timeout = 10
-my_secret = os.environ['db_password']
 connection = pymysql.connect(
   charset="utf8mb4",
   connect_timeout=timeout,
   cursorclass=pymysql.cursors.DictCursor,
   db="defaultdb",
   host="mysql-smartsys-smartsys-db.k.aivencloud.com",
-  password=my_secret,
+  password="************************",
   read_timeout=timeout,
   port=24350,
   user="avnadmin",
