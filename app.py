b93e1416afbb95d8599470b79f4a42977313abc4
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-10-19 14:18:57 +0000

    edited importation of stddb file into app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, request, redirect, url_for, flash, session
-from . import stddb
+import stddb
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
