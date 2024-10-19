e0b3dec7719d74334df02b99335e225fcaec90ff
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-10-19 14:53:02 +0000

    update close function in stddb.py

--- stddb.py
+++ stddb.py
@@ -2,23 +2,21 @@ from flask import current_app, g
 import mysql.connector
 import os
 
+
 def getdb():
     if 'db' not in g or not g.db.is_connected():
         g.db = mysql.connector.connect(
             host=os.environ['host'],  # Accessing the environment variable
             user=os.environ['user'],  # Accessing the environment variable
-            password=os.environ['password'],  # Accessing the environment variable
+            password=os.
+            environ['password'],  # Accessing the environment variable
             database=os.environ['login'],  # Accessing the environment variable
-            ssl_verify_identity=True,  # This may depend on your database settings
+            ssl_verify_identity=
+            True,  # This may depend on your database settings
         )
     return g.db
-
-def close_db(e=None):
+def closedb(e=None):
     db = g.pop('db', None)
-
-    if db is not None and db.is_connected():
+    if db is not None:
         db.close()
-
-# Close the database connection when the app context ends
-current_app.teardown_appcontext(close_db)
-
+    
