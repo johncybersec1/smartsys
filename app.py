a410bc433e536c57c97b920d201b9bbae24ba4eb
John Peter <106468893+johncybersec1@users.noreply.github.com>
2024-10-20 00:04:50 +0000

    removed load

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 
 app.secret_key = os.urandom(24) 
 
-# Load environment variables from a .env file
-load_dotenv('/etc/secrets/db_config.env')
-
 #Use pyMysSQL ofr the MySQL connection
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['db_config']
 db = SQLAlchemy(app)
